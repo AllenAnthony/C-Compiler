1ff32bf6c5ac9bba4621273816adcd3a8f41299d
hopen <yhp9523@qq.com>
2017-05-30 22:58:11 +0800

    auto commit

--- abs.cpp
+++ abs.cpp
@@ -1,28 +1,28 @@
 #include "abs.hpp"
 
 ABS_ID F_ABS_ID(char *id) {
-    ABS_ID abs_id = (ABS_ID) check_malloc(sizeof(*abs_id));
+    ABS_ID abs_id = (ABS_ID) check_malloc(sizeof(_ABS_FVAL));
     abs_id->id = string(id);
     return abs_id;
 }
 
 
 ABS_IVAL F_ABS_IVAL(int ival) {
-    ABS_IVAL abs_ival = (ABS_IVAL) check_malloc(sizeof(*abs_ival));
+    ABS_IVAL abs_ival = (ABS_IVAL) check_malloc(sizeof(_ABS_IVAL));
     abs_ival->ival = ival;
     return abs_ival;
 }
 
 
 ABS_FVAL F_ABS_FVAL(float fval) {
-    ABS_FVAL abs_fval = (ABS_FVAL) check_malloc(sizeof(*abs_fval));
+    ABS_FVAL abs_fval = (ABS_FVAL) check_malloc(sizeof(_ABS_FVAL));
     abs_fval->fval = fval;
     return abs_fval;
 }
 
 
 ABS_CVAL F_ABS_CVAL(char cval) {
-    ABS_CVAL abs_cval = (ABS_CVAL) check_malloc(sizeof(*abs_cval));
+    ABS_CVAL abs_cval = (ABS_CVAL) check_malloc(sizeof(_ABS_CVAL));
     abs_cval->cval = cval;
     return abs_cval;
 }
@@ -32,7 +32,7 @@ ABS_expression_list F_ABS_expression_list(
         ABS_expression expression
 ) {
     if (expression_list == NULL) {
-        expression_list = (ABS_expression_list) check_malloc(sizeof(*expression_list));
+        expression_list = (ABS_expression_list) check_malloc(sizeof(_ABS_expression_list));
     }
     expression_list->expression_list.push_back(expression);
     return expression_list;
@@ -44,7 +44,7 @@ ABS_expression F_ABS_expression(
         ABS_assignment_expression assignment_expression,
         ABS_compound_expression compound_expression
 ) {
-    ABS_expression expression = (ABS_expression) check_malloc(sizeof(*expression));
+    ABS_expression expression = (ABS_expression) check_malloc(sizeof(_ABS_expression));
     switch (type) {
         case ENUM_assignment_expression:
             assert(assignment_expression);
@@ -67,7 +67,7 @@ ABS_assignment_expression F_ABS_assignment_expression(
         ABS_compound_expression compound_expression_value
 ) {
     ABS_assignment_expression assignment_expression = (ABS_assignment_expression) check_malloc(
-            sizeof(*assignment_expression));
+            sizeof(_ABS_assignment_expression));
     assignment_expression->abs_id = abs_id;
     assignment_expression->compound_expression_index = compound_expression_index;
     assignment_expression->compound_expression_value = compound_expression_value;
@@ -80,7 +80,7 @@ ABS_primary_expression F_ABS_primary_expression(
         ABS_compound_expression compound_expression,
         ABS_function_invoking function_invoking
 ) {
-    ABS_primary_expression primary_expression = (ABS_primary_expression) check_malloc(sizeof(*primary_expression));
+    ABS_primary_expression primary_expression = (ABS_primary_expression) check_malloc(sizeof(_ABS_primary_expression));
     switch (type) {
         case ENUM_ID:
             primary_expression->id = id;
@@ -108,7 +108,7 @@ ABS_constant F_ABS_constant(
         ABS_FVAL abs_fval,
         ABS_CVAL abs_cval
 ) {
-    ABS_constant constant = (ABS_constant) check_malloc(sizeof(*constant));
+    ABS_constant constant = (ABS_constant) check_malloc(sizeof(_ABS_constant));
     switch (type) {
         case ENUM_IVAL:
             constant->abs_ival = abs_ival;
@@ -131,7 +131,7 @@ ABS_function_invoking F_ABS_function_invoking(
         ABS_ID abs_id,
         ABS_argue_list argue_list
 ) {
-    ABS_function_invoking function_invoking = (ABS_function_invoking) check_malloc(sizeof(*function_invoking));
+    ABS_function_invoking function_invoking = (ABS_function_invoking) check_malloc(sizeof(_ABS_function_invoking));
     function_invoking->abs_id = abs_id;
     function_invoking->argue_list = argue_list;
     return function_invoking;
@@ -139,7 +139,7 @@ ABS_function_invoking F_ABS_function_invoking(
 
 ABS_argue_list F_ABS_argue_list(ABS_argue_list argue_list, ABS_ID abs_id) {
     if (argue_list == NULL) {
-        argue_list = (ABS_argue_list) check_malloc(sizeof(*argue_list));
+        argue_list = (ABS_argue_list) check_malloc(sizeof(_ABS_argue_list));
     }
     argue_list->list.push_back(abs_id);
     return argue_list;
@@ -151,7 +151,8 @@ ABS_compound_expression F_ABS_compound_expression(
         ABS_primary_expression primary_expression,
         ABS_compound_expression compound_exp
 ) {
-    ABS_compound_expression compound_expression = (ABS_compound_expression) check_malloc(sizeof(*compound_expression));
+    ABS_compound_expression compound_expression = (ABS_compound_expression) check_malloc(
+            sizeof(_ABS_compound_expression));
     compound_expression->oper_type = oper_type;
     compound_expression->primary_expression = primary_expression;
     compound_expression->compound_expression = compound_exp;
@@ -162,7 +163,7 @@ ABS_declaration_list F_ABS_declaration_list(
         ABS_declaration_list declaration_list,
         ABS_declaration declaration) {
     if (declaration_list == NULL) {
-        declaration_list = (ABS_declaration_list) check_malloc(sizeof(*declaration_list));
+        declaration_list = (ABS_declaration_list) check_malloc(sizeof(_ABS_declaration_list));
     }
     declaration_list->declaration_list.push_back(declaration);
     return declaration_list;
@@ -171,7 +172,7 @@ ABS_declaration_list F_ABS_declaration_list(
 
 ABS_declaration F_ABS_declaration(ABS_specifier type_specifier,
                                   ABS_init_declarator_list init_declarator_list) {
-    ABS_declaration declaration = (ABS_declaration) check_malloc(sizeof(*declaration));
+    ABS_declaration declaration = (ABS_declaration) check_malloc(sizeof(_ABS_declaration));
     declaration->type_specifier = type_specifier;
     declaration->init_declarator_list = init_declarator_list;
     return declaration;
@@ -179,7 +180,7 @@ ABS_declaration F_ABS_declaration(ABS_specifier type_specifier,
 
 
 ABS_specifier F_ABS_specifier(ENUM_specifier type) {
-    ABS_specifier type_specifier = (ABS_specifier) check_malloc(sizeof(*type_specifier));
+    ABS_specifier type_specifier = (ABS_specifier) check_malloc(sizeof(_ABS_specifier));
     type_specifier->type = type;
     return type_specifier;
 }
@@ -190,7 +191,7 @@ ABS_init_declarator_list F_ABS_init_declarator_list(
 ) {
     if (init_declarator_list == NULL) {
         init_declarator_list = (ABS_init_declarator_list) check_malloc(
-                sizeof(*init_declarator_list));
+                sizeof(_ABS_init_declarator_list));
     }
     init_declarator_list->init_declarator_list.push_back(init_declarator);
     return init_declarator_list;
@@ -201,7 +202,7 @@ ABS_init_declarator F_ABS_init_declarator(
         ABS_ID id,
         ABS_constant constant
 ) {
-    ABS_init_declarator init_declarator = (ABS_init_declarator) check_malloc(sizeof(*init_declarator));
+    ABS_init_declarator init_declarator = (ABS_init_declarator) check_malloc(sizeof(_ABS_init_declarator));
     init_declarator->id = id;
     init_declarator->constant = constant;
     return init_declarator;
@@ -214,7 +215,7 @@ ABS_statement_list F_ABS_statement_list(
 ) {
     if (statement_list == NULL) {
         statement_list = (ABS_statement_list) check_malloc(
-                sizeof(*statement_list));
+                sizeof(_ABS_statement_list));
     }
     statement_list->statement_list.push_back(statement);
     return statement_list;
@@ -229,7 +230,7 @@ ABS_statement F_ABS_statement(
         ABS_jump_statement jump_statement,
         ABS_compound_expression compound_expression
 ) {
-    ABS_statement statement = (ABS_statement) check_malloc(sizeof(*statement));
+    ABS_statement statement = (ABS_statement) check_malloc(sizeof(_ABS_statement));
     statement->type = type;
     switch (type) {
         case ENUM_block_statement:
@@ -261,7 +262,7 @@ ABS_block_statement F_ABS_block_statement(
         ABS_declaration_list declaration_list,
         ABS_statement_list statement_list
 ) {
-    ABS_block_statement block_statement = (ABS_block_statement) check_malloc(sizeof(*block_statement));
+    ABS_block_statement block_statement = (ABS_block_statement) check_malloc(sizeof(_ABS_block_statement));
     block_statement->declaration_list = declaration_list;
     block_statement->statement_list = statement_list;
     return block_statement;
@@ -271,7 +272,7 @@ ABS_expression_statement F_ABS_expression_statement(
         ABS_expression_list expression_list
 ) {
     ABS_expression_statement expression_statement = (ABS_expression_statement) check_malloc(
-            sizeof(*expression_statement));
+            sizeof(_ABS_expression_statement));
     expression_statement->expression_list = expression_list;
     return expression_statement;
 }
@@ -282,7 +283,8 @@ ABS_selection_statement F_ABS_selection_statement(
         ABS_statement statement_if,
         ABS_statement statement_else
 ) {
-    ABS_selection_statement selection_statement = (ABS_selection_statement) check_malloc(sizeof(*selection_statement));
+    ABS_selection_statement selection_statement = (ABS_selection_statement) check_malloc(
+            sizeof(_ABS_selection_statement));
     selection_statement->expression_list = expression_list;
     selection_statement->statement_if = statement_if;
     selection_statement->statement_else = statement_else;
@@ -298,7 +300,8 @@ ABS_iteration_statement F_ABS_iteration_statement(
         ABS_statement statement
 
 ) {
-    ABS_iteration_statement iteration_statement = (ABS_iteration_statement) check_malloc(sizeof(*iteration_statement));
+    ABS_iteration_statement iteration_statement = (ABS_iteration_statement) check_malloc(
+            sizeof(_ABS_iteration_statement));
     iteration_statement->expression_list_while = expression_list_while;
     iteration_statement->expression_statement_for_left = expression_statement_for_left;
     iteration_statement->expression_statement_for_middle = expression_statement_for_middle;
@@ -312,7 +315,7 @@ ABS_jump_statement F_ABS_jump_statement(
         ENUM_action_type action_type,
         ABS_expression_statement expression_statement
 ) {
-    ABS_jump_statement jump_statement = (ABS_jump_statement) check_malloc(sizeof(*jump_statement));
+    ABS_jump_statement jump_statement = (ABS_jump_statement) check_malloc(sizeof(_ABS_jump_statement));
     jump_statement->action_type = action_type;
     jump_statement->expression_statement = expression_statement;
     return jump_statement;
@@ -323,7 +326,7 @@ ABS_program F_ABS_program(
         ABS_declaration_list declaration_list,
         ABS_function_definition_list function_definition_list
 ) {
-    ABS_program program = (ABS_program) malloc(sizeof(*program));
+    ABS_program program = (ABS_program) malloc(sizeof(_ABS_program));
     program->declaration_list = declaration_list;
     program->function_definition_list = function_definition_list;
     return program;
@@ -335,7 +338,7 @@ ABS_function_definition_list F_ABS_function_definition_list(
         ABS_function_definition function_definition
 ) {
     if (function_definition_list == NULL) {
-        function_definition_list = (ABS_function_definition_list) check_malloc(sizeof(*function_definition_list));
+        function_definition_list = (ABS_function_definition_list) check_malloc(sizeof(_ABS_function_definition_list));
     }
     function_definition_list->function_definition_list.push_back(function_definition);
     return function_definition_list;
@@ -347,7 +350,8 @@ ABS_function_definition F_ABS_function_definition(
         ABS_parameter_list parameter_list,
         ABS_block_statement block_statement
 ) {
-    ABS_function_definition function_definition = (ABS_function_definition) check_malloc(sizeof(*function_definition));
+    ABS_function_definition function_definition = (ABS_function_definition) check_malloc(
+            sizeof(_ABS_function_definition));
     function_definition->type_specifier = type_specifier;
     function_definition->id = id;
     function_definition->parameter_list = parameter_list;
@@ -361,7 +365,7 @@ ABS_parameter_list F_ABS_parameter_list(
         ABS_parameter parameter
 ) {
     if (parameter_list == NULL) {
-        parameter_list = (ABS_parameter_list) check_malloc(sizeof(*parameter_list));
+        parameter_list = (ABS_parameter_list) check_malloc(sizeof(_ABS_parameter_list));
     }
     parameter_list->parameter_list.push_back(parameter);
     return parameter_list;
@@ -372,7 +376,7 @@ ABS_parameter F_ABS_parameter(
         ABS_specifier type_specifier,
         ABS_ID id
 ) {
-    ABS_parameter parameter = (ABS_parameter) check_malloc(sizeof(*parameter));
+    ABS_parameter parameter = (ABS_parameter) check_malloc(sizeof(_ABS_parameter));
     parameter->type_specifier = type_specifier;
     parameter->id = id;
     return parameter;
