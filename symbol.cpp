ee501eb55b3a9bcfbceb54b4b7aa6e0ec084580f
hopen <yhp9523@qq.com>
2017-06-01 14:33:24 +0800

    auto commit

--- symbol.cpp
+++ symbol.cpp
@@ -1,80 +1,68 @@
 #include "symbol.hpp"
 
-class SymbolTable {
 
-private:
-    int size;
-    int current_depth;
-    Bucket buckets;
-    std::vector<Symbol> record;
+size_t SymbolTable::hash(std::string id) {
+    unsigned int hashValue = 0;
+    for (int i = 0; i < id.length(); i++) {
+        char temp = id[i];
+        hashValue = (hashValue << 5) + temp;
+    }
+    return hashValue % size;
+}
 
-    size_t hash(std::string id) {
-        unsigned int hashValue = 0;
-        for (int i = 0; i < id.length(); i++) {
-            char temp = id[i];
-            hashValue = (hashValue << 5) + temp;
+SymbolTable::SymbolTable(int size) {
+    this->size = size;
+    this->current_depth = 0;
+    this->buckets = (Bucket) check_malloc(sizeof(struct _Bucket) * size);
+}
+
+Symbol SymbolTable::find(std::string id) {
+    size_t index = hash(id);
+    std::vector<Symbol> symbols = buckets[index].symbols;
+    for (int i = 0; i < symbols.size(); i++) {
+        if (id == symbols[i]->id) {
+            return symbols[i];
         }
-        return hashValue%size;
-//        std::hash<std::string> hash_fn;
-//        return hash_fn(id) % size;
     }
+    return NULL;
+}
 
+Symbol SymbolTable::link(std::string id, ENUM_specifier type) {
+    size_t index = hash(id);
+    std::vector<Symbol> symbols = buckets[index].symbols;
 
-public:
-    SymbolTable(int size) {
-        this->size = size;
-        this->current_depth = 0;
-        this->buckets = (Bucket) check_malloc(sizeof(struct _Bucket) * size);
-    }
-
-    Symbol find(std::string id) {
-        size_t index = hash(id);
-        std::vector<Symbol> symbols = symbol_table[index].symbols;
-        for (int i = 0; i < symbols.size(); i++) {
-            if (id == symbols[i]->id) {
-                return symbols[i];
-            }
+    for (int i = 0; i < symbols.size(); i++) {
+        if (id == symbols[i]->id) {
+            return symbols[i];
         }
-        return NULL;
     }
 
-    Symbol link(std::string id, ENUM_specifier type) {
-        size_t index = hash(id);
-        std::vector<Symbol> symbols = symbol_table[index].symbols;
+    Symbol symbol = (Symbol) check_malloc(sizeof(_Symbol));
+    symbol->id = id;
+    symbol->type = type;
+    symbol->depth = current_depth;
 
-        for (int i = 0; i < symbols.size(); i++) {
-            if (id == symbols[i]->id) {
-                return symbols[i];
-            }
-        }
+    symbols.push_back(symbol);
+    record.push_back(symbol);
 
-        Symbol symbol = (Symbol) check_malloc(sizeof(_Symbol));
-        symbol->id = id;
-        symbol->type = type;
-        symbol->depth = current_depth;
+    return symbol;
+}
 
-        symbols.push_back(symbol);
-        record.push_back(symbol);
+void SymbolTable::enterScope() {
+    current_depth++;
+}
 
-        return symbol;
+void SymbolTable::escapeScope() {
+    size_t current_size = record.size();
+    while (current_size != 0 && record[current_size]->depth == current_depth) {
+        Symbol symbol = record.back();
+        record.pop_back();
+        size_t index = hash(symbol->id);
+        std::vector<Symbol> symbols = buckets[index].symbols;
+        symbols.pop_back();
+        current_size--;
     }
+    current_depth--;
+}
 
-    void enterScope() {
-        current_depth++;
-    }
-
-    void escapeScope() {
-        size_t current_size = record.size();
-        while (current_size != 0 && record[current_size]->depth == current_depth) {
-            Symbol symbol = record.back();
-            record.pop_back();
-            size_t index = hash(symbol->id);
-            std::vector<Symbol> symbols = symbol_table[index].symbols;
-            symbols.pop_back();
-            current_size--;
-        }
-        current_depth--;
-    }
-
-};
 
--- symbol.hpp
+++ symbol.hpp
@@ -32,62 +32,6 @@ private:
 
 
 public:
-<<<<<<< HEAD
-    SymbolTable(int size) {
-        this->size = size;
-        this->current_depth = 0;
-        this->buckets = (Bucket) check_malloc(sizeof(struct _Bucket) * size);
-    }
-
-    Symbol find(std::string id) {
-        size_t index = hash(id);
-        std::vector<Symbol> symbols = symbol_table[index].symbols;
-        for (int i = 0; i < symbols.size(); i++) {
-            if (id == symbols[i]->id) {
-                return symbols[i];
-            }
-        }
-        return NULL;
-    }
-
-    Symbol link(std::string id, ENUM_specifier type) {
-        size_t index = hash(id);
-        std::vector<Symbol> symbols = symbol_table[index].symbols;
-
-        for (int i = 0; i < symbols.size(); i++) {
-            if (id == symbols[i]->id) {
-                return symbols[i];
-            }
-        }
-
-        Symbol symbol = (Symbol) check_malloc(sizeof(_Symbol));
-        symbol->id = id;
-        symbol->type = type;
-        symbol->depth = current_depth;
-
-        symbols.push_back(symbol);
-        record.push_back(symbol);
-
-        return symbol;
-    }
-
-    void enterScope() {
-        current_depth++;
-    }
-
-    void escapeScope() {
-        size_t current_size = record.size();
-        while (current_size != 0 && record[current_size]->depth == current_depth) {
-            Symbol symbol = record.back();
-            record.pop_back();
-            size_t index = hash(symbol->id);
-            std::vector<Symbol> symbols = symbol_table[index].symbols;
-            symbols.pop_back();
-            current_size--;
-        }
-        current_depth--;
-    }
-=======
     SymbolTable(int size);
 
     Symbol find(std::string id);
@@ -97,8 +41,6 @@ public:
     void enterScope();
 
     void escapeScope();
->>>>>>> 93cf02995d1203139043ddba6c1d7c694759b722
-
 };
 
 
